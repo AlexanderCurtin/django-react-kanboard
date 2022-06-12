28c05e1cefb5880841560635d597163fa5a01bf0
Alex Curtin <alexander.curtin@temple.edu>
2022-06-12 14:30:55 -0400

    Some auth

--- django-board-client/src/App.js
+++ django-board-client/src/App.js
@@ -3,8 +3,22 @@ import './App.css';
 import { BoardList } from './layout/BoardList';
 import { Route, Router, Routes } from 'react-router-dom';
 import { Board } from './layout/Board';
+import {useAuthentication} from './contexts/AuthContext';
+import {CallbackComponent} from './Pages/CallbackComponent'
+import { LoginComponent } from './Pages/LoginComponent';
 
 function App() {
+  console.log(useAuthentication());
+  const {loaded, user} = useAuthentication();
+  if(!loaded){
+    return <>Loading...</>
+  }
+  if(!user){
+    return (<Routes>
+        <Route path="/callback" exact element={<CallbackComponent/>} />
+        <Route path="/*" element={<LoginComponent/>}></Route>
+      </Routes>)
+  }
   return (
     <div className="App">
       <header className="App-header">
